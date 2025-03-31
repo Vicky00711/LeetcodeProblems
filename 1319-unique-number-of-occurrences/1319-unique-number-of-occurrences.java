c13909666e48c6c33ab0fd0a69b18ad1975df396
Prateek sahoo <122237544+Vicky00711@users.noreply.github.com>
2025-03-31 18:20:21 +0100

    Time: 5 ms (6.3%), Space: 42.3 MB (23.19%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,46 +1,30 @@
 class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
+    public  boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> map= new HashMap<>();
         for (int i=0; i<arr.length; i++){
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
-            // if (map.containsKey(arr[i])){
-            //     int freq=map.get(arr[i]);
-            //     map.put(arr[i], freq+1);
-            // }
-            // else {
-            //     map.put(arr[i], 1);
-            // }
+            if (!map.containsKey(arr[i])){
+                map.put(arr[i], 1);
+            }
+            else {
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+
         }
 
-        
+        System.out.println(map);
 
-        // HashMap<Integer, Integer> map2= new HashMap<>();
-        // for (Integer keys: map.keySet()) {
-        //     if (map2.containsKey(map.get(keys))){
-        //         int freq=map2.get(map.get(keys));
-        //         map2.put(map.get(keys), freq+1);
-        //     }
-        //     else {
-        //         map2.put(map.get(keys), 1);
-        //     }
-        // }
-    
-        HashSet<Integer> set= new HashSet<>();
-        for (Integer values:map.values()){
-            set.add(values);
+        Set<Integer> set= new HashSet<>();
+        for (Integer keys:map.keySet()){
+             set.add(map.get(keys));
         }
 
-        if (map.size()==set.size()){
+        if (set.size()==map.size()){
             return true;
         }
-        else{
+
+        else {
             return false;
         }
-        // for(Integer values: map2.values()){
-        //     if (values>1){
-        //         return false;
-        //     }
-        // }
-        // return true;
+
     }
 }
